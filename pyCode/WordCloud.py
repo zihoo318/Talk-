733a3ad1418534c8459ea934709bff4aaa1df11e
zihoo318 <147166955+zihoo318@users.noreply.github.com>
2024-06-28 20:43:00 +0900

    Update WordCloud.py
    수정된 전처리에 맞춰서 수정

--- pyCode/WordCloud.py
+++ pyCode/WordCloud.py
@@ -1,6 +1,7 @@
 #워드클라우드 생성
 #필터링 하지 않고 전처리과정에서 오타만 거른 파일로 그대로 워드클라운드 생성
 # 최소 빈도수 이상인 단어들만 필터링 현재 min_frequency=25
+
 from collections import Counter
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
@@ -11,19 +12,29 @@ def generate_wordcloud_from_file(personal_file_path):
         print(f"파일이 존재하지 않습니다: {personal_file_path}")
         return
 
+    # 제외할 단어 목록
+    excluded_words = {'안', '아'}
+
     # 개인 파일에서 형태소 분석 결과 읽기
     with open(personal_file_path, 'r', encoding='utf-8') as file:
-        morphs = file.read().split()
+        content = file.read()
+        
+    # ' ;'를 제거하고 형태소 분석 결과를 분리
+    morphs = content.replace(' ;', ' ').replace(';', '').split()
+
+    # 제외할 단어를 걸러낸 형태소 리스트
+    morphs = [word for word in morphs if word not in excluded_words and word.strip() != '']
 
     # 형태소 빈도 계산
     word_counts = Counter(morphs)
+    
     # 최소 빈도수 이상인 단어들만 필터링
-    min_frequency=25
+    min_frequency = 20
     filtered_word_counts = {word: count for word, count in word_counts.items() if count >= min_frequency}
 
     # 워드 클라우드 생성
     wordcloud = WordCloud(font_path='/usr/share/fonts/truetype/nanum/NanumGothic.ttf',
-                          background_color='white', width=800, height=600).generate_from_frequencies(word_counts)
+                          background_color='white', width=400, height=300).generate_from_frequencies(filtered_word_counts)
 
     # 워드 클라우드 출력
     plt.figure(figsize=(10, 8))
@@ -46,5 +57,5 @@ file_path = '/content/KakaoTalk.txt'
 user_name = '강지후'
 personal_file_path = chat_room.get_personal_file_path(user_name)
 generate_wordcloud_from_file(personal_file_path)
-#워드클라우드 group 출력
-#generate_wordcloud_from_file('/content/group.txt')
+# 워드클라우드 group 출력
+# generate_wordcloud_from_file('/content/group.txt')
